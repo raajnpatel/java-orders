fd74850abf6baa365087544cddd3db5bc8a15304
rndpatel@gmail.com <rndpatel@gmail.com>
2019-10-09 16:32:27 -0700

    initial commit - project initiated

---
+++ javaorders/src/main/java/local/raajn/javaorder/JavaorderApplication.java
@@ -0,0 +1,15 @@
+package local.raajn.javaorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaorderApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(JavaorderApplication.class, args);
+    }
+
+}
