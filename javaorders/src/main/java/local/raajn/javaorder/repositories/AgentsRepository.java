dc0d561cc1e5957058ec870d2b26a0b0d8e7107c
rndpatel@gmail.com <rndpatel@gmail.com>
2019-10-09 22:41:32 -0700

    MVP Complete -  fixed my issue with SeedData

--- javaorders/src/main/java/local/raajn/javaorder/repositories/AgentsRepository.java
+++ javaorders/src/main/java/local/raajn/javaorder/repositories/AgentsRepository.java
@@ -3,6 +3,7 @@ package local.raajn.javaorder.repositories;
 import local.raajn.javaorder.models.Agents;
 import org.springframework.data.repository.CrudRepository;
 
+
 public interface AgentsRepository extends CrudRepository<Agents, Long>
 {
 
--- javaorders/src/main/java/local/raajn/javaorder/services/CustomerServiceImpl.java
+++ javaorders/src/main/java/local/raajn/javaorder/services/CustomerServiceImpl.java
@@ -7,10 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
-
-
 @Transactional
 @Service(value = "customerService")
 public class CustomerServiceImpl implements CustomerService
@@ -53,14 +52,62 @@ public class CustomerServiceImpl implements CustomerService
     }
     @Transactional
     @Override
-    public Customers update(Customers customers, long custcode)
-    {
-        return null;
+    public Customers update(Customers customer, long id) {
+        Customers currentCustomer = custrepos.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(Long.toString(id)));
+
+        if (customer.getCustname() != null) {
+            currentCustomer.setCustname(customer.getCustname());
+        }
+        if (customer.getCustcity() != null) {
+            currentCustomer.setCustcity(customer.getCustcity());
+        }
+        if (customer.getWorkingarea() != null) {
+            currentCustomer.setWorkingarea(customer.getWorkingarea());
+        }
+        if (customer.getCustcountry() != null) {
+            currentCustomer.setCustcountry(customer.getCustcountry());
+        }
+        if (customer.getGrade() != null) {
+            currentCustomer.setGrade(customer.getGrade());
+        }
+        if (customer.getOpeningamt() != 0) {
+            currentCustomer.setOpeningamt(customer.getOpeningamt());
+        }
+        if (customer.getReceiveamt() != 0) {
+            currentCustomer.setReceiveamt(customer.getReceiveamt());
+        }
+        if (customer.getPaymentamt() != 0) {
+            currentCustomer.setPaymentamt(customer.getPaymentamt());
+        }
+        if (customer.getOutstandingamt() != 0) {
+            currentCustomer.setOutstandingamt(customer.getOutstandingamt());
+        }
+        if (customer.getPhone() != null) {
+            currentCustomer.setPhone(customer.getPhone());
+        }
+
+        // added
+        if (customer.getAgent() != null) {
+            currentCustomer.setAgent(customer.getAgent());
+        }
+
+        if (customer.getOrders().size() > 0) {
+            for (Orders o : customer.getOrders()) {
+                currentCustomer.getOrders().add(new Orders(o.getOrdamount(), o.getAdvanceamount(), currentCustomer,
+                        o.getOrddescription()));
+            }
+        }
+
+        return custrepos.save(currentCustomer);
     }
 
     @Override
-    public void delete(long custcode)
-    {
-
+    public void delete(long id) {
+        if (custrepos.findById(id).isPresent()) {
+            custrepos.deleteById(id);
+        } else {
+            throw new EntityNotFoundException("Id " + id);
+        }
     }
 }
